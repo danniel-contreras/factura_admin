19f2d7af154f0cd994891a7981f03fb56d7407ae
daniel-contreras <contrerasdannniel@gmail.com>
2023-04-07 09:25:04 -0600

    Project structure and login page

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import Login from "./pages/auth/Login.vue"
+</script>
+
+<template>
+ <Login/>
+</template>
---
+++ src/plugins/vue-icons.ts
@@ -0,0 +1,7 @@
+import { OhVueIcon, addIcons } from "oh-vue-icons";
+import { FaUserCircle,FaLock } from "oh-vue-icons/icons"
+
+
+addIcons(FaUserCircle,FaLock);
+
+export default OhVueIcon;
